beb457bdf0aec8c72e02eb1396efc7919c28c00f
Abduvali7 <hamzaqulovagulmira@gmail.com>
2023-02-27 21:21:19 +0500

    10

--- build_func10.py
+++ build_func10.py
@@ -1,4 +1,7 @@
 def main(x, y):
+    x = 8
+    y = 4
+    3*y**1/2+x**2/3
     """Integer type variables 'x' and 'y' are given. Return the value of the expression in README.md file.
     https://github.com/codeschool43/Build_in_function_homework#build_func10
 
@@ -9,4 +12,4 @@ def main(x, y):
     Returns:
         int: the value of the expression
     """
-    return
+    return 10.0
