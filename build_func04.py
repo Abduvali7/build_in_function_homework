090f27bfe7339587768811b41ce0e75c8077358b
Abduvali7 <hamzaqulovagulmira@gmail.com>
2023-02-27 21:08:53 +0500

    04

--- build_func04.py
+++ build_func04.py
@@ -1,5 +1,6 @@
 def main(n):
     n = 4
+    2+n/3**2
     """A integer type variable 'n' is given. Return the value of the expression in README.md file.
     https://github.com/codeschool43/Build_in_function_homework#build_func04 
 
